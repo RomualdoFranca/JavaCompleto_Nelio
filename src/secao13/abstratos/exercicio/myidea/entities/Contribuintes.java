7b144d771c8bae9a1569249fb33f06bb8b8d3dba
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-08-31 21:24:16 -0300

    cria as classes do exercicio de imposto sobre pessoa fisica e juridica

--- src/secao13/abstratos/exercicio/myidea/entities/Contribuintes.java
+++ src/secao13/abstratos/exercicio/myidea/entities/Contribuintes.java
@@ -1,5 +1,33 @@
 package secao13.abstratos.exercicio.myidea.entities;
 
 public abstract class Contribuintes {
+	private final String nome;
+	private Double rendaAnual;
+	
+	public Contribuintes() {
+		this.nome = "";
+		
+	}
+
+	public Contribuintes(String nome, Double rendaAnual) {
+		this.nome = nome;
+		this.rendaAnual = rendaAnual;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+
+	public void setRendaAnual(double rendaAnual) {
+		this.rendaAnual = rendaAnual;
+	}
+	
+	public Double getRendaAnual() {
+		return rendaAnual;
+	}
+	
+	public abstract double calculaImposto();
+	
 
 }
--- src/secao13/abstratos/exercicio/myidea/entities/PessoaFisica.java
+++ src/secao13/abstratos/exercicio/myidea/entities/PessoaFisica.java
@@ -1,5 +1,48 @@
 package secao13.abstratos.exercicio.myidea.entities;
 
-public class PessoaFisica {
+public class PessoaFisica extends Contribuintes{
+
+	private Double gastosSaude;
+	
+	public PessoaFisica() {
+		super();
+	}
+	
+	public PessoaFisica(String nome, Double rendaAnual, Double gastosSaude) {
+		super(nome, rendaAnual);
+		this.gastosSaude = gastosSaude;
+	}
+
+	
+	public Double getGastosSaude() {
+		return gastosSaude;
+	}
+
+	public void setGastosSaude(Double gastosSaude) {
+		this.gastosSaude = gastosSaude;
+	}
+
+	@Override
+	public double calculaImposto() {
+		
+		if(getRendaAnual() < 20.000) {
+			
+			double taxaImpostoRenda = 0.0;
+			double valorImpostoSaude = getGastosSaude() * 0.5;
+			
+			if(getGastosSaude() > 0.0) {
+				taxaImpostoRenda = 0.15;
+				double valorImpostoRenda = getRendaAnual() * 0.15;
+				
+				return getRendaAnual() - (valorImpostoRenda - valorImpostoSaude);
+			}else {
+				return getRendaAnual() - valorImpostoRenda;
+			}
+			
+		}else {
+			
+		}
+		return 0;
+	}
 
 }
