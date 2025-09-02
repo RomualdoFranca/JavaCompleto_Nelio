84ed7baae5f7dfade1018e707637e5d45517acdd
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-09-01 21:43:47 -0300

    cria pacotes da secao14 sobre exceções. Implementa um main para testar os tipos de exceções

---
+++ src/secao14/tratamento_excecoes/trycatch/TryCatchTheory.java
@@ -0,0 +1,43 @@
+package secao14.tratamento_excecoes.trycatch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TryCatchTheory {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		//um vetor de string que vai receber um nextLine().split(" ") baseado num espaço em branco
+		//ler varios dados na mesma linha separado por espaço em branco e cada um desses dados vai ser um elemento do vetor
+//		String[] vect =  sc.nextLine().split(" ");
+//		//leitura de uma variavel position
+//		int position = Integer.parseInt(sc.nextLine());
+//		//mostra na tela o vetor na posição informada
+//		System.out.println(vect[position]);
+		
+		//testes para gerar exceções: inserir um tipo diferente na variavel (position)
+		//tentar acessar um valor maior que o tamanho do vetor.
+		//por exemplo:ao inserir 3 nomes no vetor, ele tera 2 como numero maximo da variavel position
+		
+		//exemplo sem os comentarios para facilitar a leitura do codigo
+		
+		try {
+			String[] vect1 =  sc.nextLine().split(" ");
+			int position1 = Integer.parseInt(sc.nextLine());
+			System.out.println(vect1[position1]);
+			//caso uma posição que não exista for inserida, a mensagem do bloco catch sera impressa
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+		
+		} catch (NumberFormatException e) {
+			System.out.println("Input error!");
+		}
+		
+		System.out.println("End of program");
+		
+		sc.close();
+	}
+
+}
