70be15ebb2cdd1202798778229858444b94dce7e
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-09-03 10:58:15 -0300

    implementa metodo estatico para receber as datas

--- src/ia/tutorial/classeDate/OperacoesDatas.java
+++ src/ia/tutorial/classeDate/OperacoesDatas.java
@@ -3,6 +3,8 @@ package ia.tutorial.classeDate;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class OperacoesDatas {
@@ -10,6 +12,16 @@ public class OperacoesDatas {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Testando método ");
+		List<LocalDate> datas = insereDatas(sc);
+		
+		//imprime as datas inseridas
+		for (LocalDate localDate : datas) {
+			System.out.println(localDate.format(dtf));
+		}
+		
 		System.out.print("Insira a data de entrada (dd/mm/yyyy):");
 		String dataInputEntrada = sc.nextLine();
 
@@ -17,7 +29,6 @@ public class OperacoesDatas {
 		String dataInputSaida = sc.nextLine();
 
 		// define o padrão de formatação de entrada
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		// o .parse() recebe o tipo string com a formação definida e converte num tipo
 		// LocalDate
 		LocalDate entrada = LocalDate.parse(dataInputEntrada, dtf);
@@ -35,9 +46,9 @@ public class OperacoesDatas {
 		LocalDate alteraEntrada = LocalDate.parse(saidaAlterada, dtf);
 		LocalDate alteraSaida = LocalDate.parse(saidaAlterada, dtf);
 		
-		if (alteraEntrada.isAfter(entrada) && ) {
-			
-		}
+//		if (alteraEntrada.isAfter(entrada) && ) {
+//			
+//		}
 		
 		
 		//loop para inserir datas nas seguintes regras:
@@ -61,6 +72,24 @@ public class OperacoesDatas {
 
 	}
 	
-
+	public static List<LocalDate> insereDatas(Scanner sc) {
+		System.out.print("Insira data 01: ");
+		String inputData01 =  sc.nextLine();
+		System.out.print("Insira data 02: ");
+		String inputData02 = sc.nextLine();
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate data01 = LocalDate.parse(inputData01, dtf);
+		LocalDate data02 = LocalDate.parse(inputData02, dtf);
+		
+		List<LocalDate> datas = new ArrayList<LocalDate>();
+		datas.add(data01);
+		datas.add(data02);
+		return datas;
+		
+		
+		
+		
+	}
 
 }
