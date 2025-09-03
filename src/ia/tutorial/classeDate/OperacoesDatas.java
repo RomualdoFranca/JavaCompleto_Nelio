812c826881ecb8e1cdce99110f619b30ccf3e58c
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-09-03 13:10:59 -0300

    refatora os metodos de inserir datas, e o metodo atualizar. Cria um arquivo .txt com a refatoração da IA.

--- src/ia/tutorial/classeDate/OperacoesDatas.java
+++ src/ia/tutorial/classeDate/OperacoesDatas.java
@@ -1,6 +1,7 @@
 package ia.tutorial.classeDate;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -12,84 +13,92 @@ public class OperacoesDatas {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		// definindo as datas
+//		LocalDate dataInicial = LocalDate.of(2025, 9, 10);
+//		LocalDate dataFinal = LocalDate.of(2025, 9, 20);
+//		
+		// define a formatação de entrada e saída da data
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		
-		System.out.println("Testando método ");
+
 		List<LocalDate> datas = insereDatas(sc);
 		
-		//imprime as datas inseridas
+		// imprime as datas inseridas
+		System.out.println("Primeira entrada de data");
 		for (LocalDate localDate : datas) {
 			System.out.println(localDate.format(dtf));
 		}
 		
-		System.out.print("Insira a data de entrada (dd/mm/yyyy):");
-		String dataInputEntrada = sc.nextLine();
+		alteraDatas(sc, dtf, datas);
+		
+		System.out.println("Segunda entrada de data");
+		for (LocalDate localDate : datas) {
+			System.out.println(localDate.format(dtf));
+		}
 
-		System.out.print("Insira a data de saída (dd/mm/yyyy):");
-		String dataInputSaida = sc.nextLine();
+//		// acessa as data pelo indice da lista
+//		System.out.println("Primeira data inserida: " + datas.get(0).format(dtf));
 
-		// define o padrão de formatação de entrada
-		// o .parse() recebe o tipo string com a formação definida e converte num tipo
-		// LocalDate
-		LocalDate entrada = LocalDate.parse(dataInputEntrada, dtf);
-		LocalDate saida = LocalDate.parse(dataInputSaida, dtf);
-		
-		System.out.println("Data de entrada: " + entrada.format(dtf));
-		System.out.println("Data de saída: " + saida.format(dtf));
-
-		System.out.print("Altera a data de entrada:");
-		String entradaAlterada = sc.nextLine();
-		
-		System.out.print("Altera a data de saída:");
-		String saidaAlterada = sc.nextLine();
-		
-		LocalDate alteraEntrada = LocalDate.parse(saidaAlterada, dtf);
-		LocalDate alteraSaida = LocalDate.parse(saidaAlterada, dtf);
-		
-//		if (alteraEntrada.isAfter(entrada) && ) {
-//			
-//		}
-		
-		
-		//loop para inserir datas nas seguintes regras:
-		//data de saida maior que data de entrada
-		//alteração da data so pode ocorrer para datas futuras em relação a data inserida inicialmente
-		
+		// loop para inserir datas nas seguintes regras:
+		// data de saida maior que data de entrada
+		// alteração da data so pode ocorrer para datas futuras em relação a data
+		// inserida inicialmente
 
 		// diferença entre as datas.
-		long periodoDias = ChronoUnit.DAYS.between(entrada, saida);
-		System.out.println("Diferença entre os dias com data inseridas pelo usuário: " + periodoDias);
-
-		// definindo as datas
-		LocalDate dataInicial = LocalDate.of(2025, 9, 10);
-		LocalDate dataFinal = LocalDate.of(2025, 9, 20);
-
+//		long periodoDias = ChronoUnit.DAYS.between(entrada, saida);
+//		System.out.println("Diferença entre os dias com data inseridas pelo usuário: " + periodoDias);
+		
 		// calculando a diferença de dias
-		long diferencaEmDias = ChronoUnit.DAYS.between(dataInicial, dataFinal);
-		System.out.println(diferencaEmDias);
+//		long diferencaEmDias = ChronoUnit.DAYS.between(dataInicial, dataFinal);
+//		System.out.println(diferencaEmDias);
 
 		sc.close();
 
+	}// fim do main
+
+	//metodo para imprimir as datas inseridas
+	public static void imprimeDatas(List<LocalDate> datas, DateTimeFormatter dtf) {
+		for (LocalDate localDate : datas) {
+			System.out.println(localDate.format(dtf));
+		}
 	}
 	
 	public static List<LocalDate> insereDatas(Scanner sc) {
 		System.out.print("Insira data 01: ");
-		String inputData01 =  sc.nextLine();
+		String inputData01 = sc.nextLine();
 		System.out.print("Insira data 02: ");
 		String inputData02 = sc.nextLine();
-		
+
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		LocalDate data01 = LocalDate.parse(inputData01, dtf);
 		LocalDate data02 = LocalDate.parse(inputData02, dtf);
-		
+
 		List<LocalDate> datas = new ArrayList<LocalDate>();
 		datas.add(data01);
 		datas.add(data02);
 		return datas;
-		
-		
-		
-		
 	}
 
-}
+	// *******************************************************
+	public static List<LocalDate> alteraDatas(Scanner sc, DateTimeFormatter dtf, List<LocalDate> datas) {
+		System.out.println("Nova data de entrada:");
+
+		for (int i = 0; i < datas.size(); i++) {
+			
+			System.out.print("Data " + (i + 1) + ": ");
+			LocalDate novaData = LocalDate.parse(sc.nextLine(), dtf);
+			boolean ehDepois = novaData.isAfter(datas.get(i));
+			
+			if (ehDepois == true){
+				System.out.println("A alteração PODE ser realizada.");
+				//realiza a alteração da data que foram inseridas na lista
+				datas.set(i, novaData);
+			} else {
+				System.out.println("A alteração NÃO pode ser realizada.");
+
+			}
+		}
+		return datas;
+
+	}// fim do metodo alteraDatas
+
+}// fim da classe
